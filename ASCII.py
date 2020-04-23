73e07f799ce17abab6a6aa1a012c9242297fe509
Grant Parkinson <grant.c.parkinson@gmail.com>
2020-04-22 17:25:54 -0700

    added print statements and main function

---
+++ ASCII.py
@@ -0,0 +1,20 @@
+"""
+CTEC 121
+<Grant Parkinson>
+<assignment/lab name>
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+
+def main():
+    print(5 + 5 + 5)
+    print(6 * 8)
+
+
+main()
---
+++ programming assignment.py
@@ -0,0 +1,21 @@
+"""
+CTEC 121
+<Grant Parkinson>
+<TASK: Programming Assignment>
+<added additional print statements>
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+
+def main():
+    print("Hello professor ")
+    print(144 / 12 * 62)
+    print(62 / 55 * 894 / 555 * 100)
+
+
+main()
